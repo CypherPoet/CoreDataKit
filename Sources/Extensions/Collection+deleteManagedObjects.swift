892281452cdcf085d133ec7dfec458e5a6e97e2a
CypherPoet <CypherPoet@gmail.com>
2020-02-25 22:16:31 -0600

    Update access

--- Sources/Extensions/Collection+deleteManagedObjects.swift
+++ Sources/Extensions/Collection+deleteManagedObjects.swift
@@ -8,7 +8,7 @@ extension Collection where
 {
     /// Delete managed object entities at a set of indices in a list -- using the `managedObjectContext`
     /// that's associated with each entity.
-    func delete(at indices: IndexSet) {
+    public func delete(at indices: IndexSet) {
         indices.forEach { index in
             let element = self[index]
             
