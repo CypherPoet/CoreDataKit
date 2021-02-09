6e2957a77e127895c3b8c19959fab717d8623789
CypherPoet <CypherPoet@gmail.com>
2021-02-08 21:01:45 -0600

    Call super.createDestinationInstances(forSource:in:manager:) instead of traversing.
    instead of manually traversing all properties, I don't see why we shouldn't let the mapping model do what it's design to do 🙂

--- Examples/Migration/ReviewJournal/Shared/Data/Migration/Policies/ImageAttachmentToImageAttachment_v3_to_v4.swift
+++ Examples/Migration/ReviewJournal/Shared/Data/Migration/Policies/ImageAttachmentToImageAttachment_v3_to_v4.swift
@@ -19,25 +19,21 @@ class ImageAttachmentToImageAttachment_v3_to_v4: NSEntityMigrationPolicy {
         in mapping: NSEntityMapping,
         manager: NSMigrationManager
     ) throws {
-//        try super.createDestinationInstances(forSource: sourceEntity, in: mapping, manager: manager)
-        
-        guard let attributeMappings = mapping.attributeMappings else {
-            throw Error.noAttributeMappingsFound
-        }
+        try super.createDestinationInstances(forSource: sourceEntity, in: mapping, manager: manager)
         
         // Get the destination entity
-        let destinationImageAttachment = try self.destinationImageAttachment(in: manager.destinationContext, using: mapping)
-        
-        
-        // Traverse property mappings
-        for propertyMapping in attributeMappings {
-            try migrateValue(
-                in: propertyMapping,
-                from: sourceEntity,
-                to: destinationImageAttachment
-            )
+        guard
+            let destinationImageAttachment = manager
+                .destinationInstances(
+                    forEntityMappingName: mapping.name,
+                    sourceInstances: [sourceEntity]
+                )
+                .first
+        else {
+            throw Error.destinationEntityNotFound
         }
         
+
         // Grab image-related properties
         if
             let sourceImageData = sourceEntity.value(forKey: "imageData") as? Data,
@@ -54,57 +50,6 @@ class ImageAttachmentToImageAttachment_v3_to_v4: NSEntityMigrationPolicy {
             reviewBodyText.prefix(80),
             forKey: "title"
         )
-
-        // Use the manager to connect the source and destination entities
-        manager.associate(
-            sourceInstance: sourceEntity,
-            withDestinationInstance: destinationImageAttachment,
-            for: mapping
-        )
-    }
-    
-    
-    func migrateValue(
-        in propertyMapping: NSPropertyMapping,
-        from sourceEntity: NSManagedObject,
-        to destinationEntity: NSManagedObject
-    ) throws {
-        guard let destinationAttributeName = propertyMapping.name else {
-            throw Error.noDestinationAttributeNameFound(propertyMapping: propertyMapping)
-        }
-        
-        // 🔑 Even though this is a custom migration, most of the attribute migrations
-        // should be performed using the expressions we've defined in the mapping model.
-        if let valueExpression = propertyMapping.valueExpression {
-            let context: NSMutableDictionary = [
-                "source": sourceEntity
-            ]
-            
-            guard let destinationValue = valueExpression.expressionValue(
-                with: sourceEntity,
-                context: context
-            ) else { return }
-            
-            destinationEntity.setValue(destinationValue, forKey: destinationAttributeName)
-        }
-    }
-    
-    
-    func destinationImageAttachment(
-        in destinationContext: NSManagedObjectContext,
-        using mapping: NSEntityMapping
-    )  throws -> ImageAttachment {
-        guard let entityName = mapping.destinationEntityName else {
-            throw Error.failedToFindEntityName
-        }
-
-        guard
-            let entityDescription = NSEntityDescription.entity(forEntityName: entityName, in: destinationContext)
-        else {
-            throw Error.failedToMakeDestinationEntityDescription
-        }
-        
-        return ImageAttachment(entity: entityDescription, insertInto: destinationContext)
     }
 }
 
@@ -113,9 +58,6 @@ class ImageAttachmentToImageAttachment_v3_to_v4: NSEntityMigrationPolicy {
 extension ImageAttachmentToImageAttachment_v3_to_v4 {
     
     enum Error: Swift.Error {
-        case failedToFindEntityName
-        case failedToMakeDestinationEntityDescription
-        case noAttributeMappingsFound
-        case noDestinationAttributeNameFound(propertyMapping: NSPropertyMapping)
+        case destinationEntityNotFound
     }
 }
